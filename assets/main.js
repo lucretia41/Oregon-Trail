f7505adbf5cc3888cff58345e46ac3e11afcf0f0
Scott Martin <scooter@ianscottmartin@gmail.com>
2020-07-10 14:45:03 -0600

    staging

---
+++ assets/main.js
@@ -0,0 +1,41 @@
+class Wagon {
+    //properties
+    constructor(passengers, capacity) {
+        this.passengers = passengers
+        this.capacity = capacity
+    }
+
+    //methods
+    getAvailableSeatCount() {
+        //code here
+    }
+    join() {
+        //code here
+    }
+    shouldQuarantine() {
+        //code here
+    }
+    totalFood() {
+        //code here
+    }
+}
+
+class traveler {
+    //properties
+    constructor(name, food, isHealthy) {
+        this.name = name
+        this.food = food
+        this.isHealthy = isHealthy
+
+    }
+    //methods
+    hunt() {
+        //code here
+    }
+    eat() {
+        //code here
+    }
+
+
+
+}
